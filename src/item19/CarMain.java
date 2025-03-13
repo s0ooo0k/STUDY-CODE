2e4536bf6612c2db3e0e95884ff640e73e9fe34a
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-03-14 04:04:25 +0900

    [fix] Car ìë수정

--- src/item19/CarMain.java
+++ src/item19/CarMain.java
@@ -3,7 +3,7 @@ package item19;
 public class CarMain {
     public static void main(String[] args) {
         Car car = new Car();
-        Car sportscar = new SportsCar();
+        SportsCar sportscar = new SportsCar();
 
         car.drive();
         sportscar.drive();
